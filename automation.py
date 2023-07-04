7d0368b0e765f85df1def0e9f818c0ff32b628b7
Jay Kueny <jkueny@MacBook-Pro-5.local>
2023-07-04 14:14:46 -0700

    Initial commit.

---
+++ automation.py
@@ -0,0 +1,495 @@
+import glob as glob
+import os
+from time import sleep
+import numpy as np
+
+try:
+    if (os.environ['HOME'].endswith('jkueny')) or (os.environ['HOME'].endswith('xsup')):
+        import h5py
+        from astropy.io import fits
+        print('Executing on Pinky...')
+except:
+    print('Executing on the 4D computer...')
+
+
+
+from fourD import capture_frame
+from bmc import load_channel, write_fits, update_voltage_2K
+# from irisao import write_ptt_command, apply_ptt_command #commented 6/27/23 JKK
+# from . import alpao #commented 6/27/23 JKK
+
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def phasecam_dm_run(
+                dm_inputs,
+                network_path,
+                outname,
+                dmtype,
+                delay=None,
+                # consolidate=True,
+                dry_run=True,
+                clobber=False,
+                mtype='measure',
+                input_name='dm_inputs.fits',
+                ):
+    '''
+    Loop over dm_inputs, setting the DM in the requested state,
+    and taking measurements on the 4D.
+
+    Ultimately we probably want to perform a "measurement" b/w
+    DM commands which produces Surface Data. The Raw Data is
+    just the unwrapped phase map, while the Surface Data has the
+    reference subtracted, Zernike removal, and masking applied.
+
+    In the outname directory, the individual measurements are
+    saved in separate .hdf5 files. 
+    
+    Consolidated measurements (surface maps, intensity maps,
+    attributes, dm inputs) are saved out to 'alldata.hdf5' under
+    this directory. (Not really done here b/c 4Sight outputs hdf5)
+    by default. At the end, we can probably use the GUI to consolidate
+    all .hdf5's if that's what works best. JKK 06/28/23)
+
+    Parameters:
+        dm_inputs: array-like
+            Cube of displacement images. The DM will iteratively
+            be set in each state on channel 0.
+        network_path : str
+            Path to shared network folder visible to both
+            Pinky and the 4D machine. This is where
+            cross-machine communication will take place.
+            Both machines must have read/write privileges.
+        dmtype : str
+            'bmc', 'irisao', or 'alpao'. This determines whether
+            the dm_inputs are written to .fits or .txt.
+        outname : str
+            Directory to write results out to. Directory
+            must not already exist.
+        delay : float, opt.
+            Time in seconds to wait between measurements.
+            Default: no delay.
+        consolidate : bool, opt.
+            Attempt to consolidate all files at the end?
+            Default: True
+        dry_run : bool, opt.
+            If toggled to True, this will loop over DM states
+            without taking images. This is useful to debugging
+            things on the DM side / watching the fringes on
+            the Zygo live monitor.
+        clobber : bool, opt.
+            Allow writing to directory that already exists?
+            Risks overwriting files that already exist, but
+            useful for interactive measurements.
+        mtype : str
+            'acquire' or 'measure'. 'Acquire' takes a measurement
+            without analyzing or updating the GUI (faster), while
+            'measure' takes a measurement, analyzes, and updates
+            the GUI (slower). JKK: Measurement is by default a 
+            10 frame acquisiton, averaged to help with bench
+            seeing.
+    Returns: nothing
+
+    '''
+    if dmtype.upper() not in ['BMC']:
+        raise ValueError('dmtype not recognized. Must be "BMC".')
+
+    if not (dry_run or clobber):
+        # Create a new directory outname to save results to
+        assert not os.path.exists(outname), '{0} already exists!'.format(outname)
+        os.mkdir(outname)
+
+    fd_mon = fourDMonitor(network_path)
+
+    for idx, inputs in enumerate(dm_inputs):
+
+        if dmtype.upper() == 'BMC':
+            #Remove any old inputs if they exist
+            old_files = glob.glob(os.path.join(network_path,'dm_input*.fits'))
+            for old_file in old_files:
+                if os.path.exists(old_file):
+                    os.remove(old_file)
+            # Write out FITS file with requested DM input
+            log.info('Setting DM to state {0}/{1}.'.format(idx + 1, len(dm_inputs)))
+            input_file = os.path.join(network_path,input_name)
+            # write out
+            # if dmtype.upper() == 'ALPAO':
+            #     alpao.command_to_fits(inputs, input_file, overwrite=True)
+            # else: #BMC
+            #     write_fits(input_file, inputs, dtype=np.float32, overwrite=True)
+            write_fits(input_file, inputs, dtype=np.float32, overwrite=True)
+        else: #IRISAO
+            print('Invalid DM type. Only BMC is currently supported for PhaseCam work.')
+            break
+        # else: #IRISAO
+        #     input_file = os.path.join(network_path,'ptt_input.txt'.format(idx))
+        #     write_ptt_command(inputs, input_file)
+
+        # Wait until DM indicates it's in the requested state
+        # I'm a little worried the DM could get there before
+        # the monitor starts watching the dm_ready file, but 
+        # that hasn't happened yet.
+        fd_mon.watch(0.01)
+        log.info('DM ready!')
+
+        if not dry_run:
+            # Take an image on the Zygo
+            log.info('Taking image!')
+            capture_frame(filename=os.path.join(outname,'frame_{0:05d}.h5'.format(idx)),
+                          mtype=mtype)
+
+        # Remove input file
+        if os.path.exists(input_file):
+            os.remove(input_file)
+
+        if delay is not None:
+            sleep(delay)
+
+    # if consolidate:
+    #     log.info('Writing to consolidated .hdf5 file.')
+    #     # Consolidate individual frames and inputs
+    #     # Don't read attributes into a dictionary. This causes python to crash (on Windows)
+    #     # when re-assignging them to hdf5 attributes.
+    #     alldata = read_many_raw_datx(sorted(glob.glob(os.path.join(outname,'frame_*.datx'))), 
+    #                                  attrs_to_dict=True, mask_and_scale=True)
+    #     write_dm_run_to_hdf5(os.path.join(outname,'alldata.hdf5'),
+    #                          np.asarray(alldata['surface']),
+    #                          alldata['surface_attrs'][0],
+    #                          np.asarray(alldata['intensity']),
+    #                          alldata['intensity_attrs'][0],
+    #                          alldata['attrs'][0],
+    #                          np.asarray(dm_inputs),
+    #                          alldata['mask'][0]
+    #                          )
+
+def write_dm_run_to_hdf5(filename, surface_cube, surface_attrs, intensity_cube,
+                         intensity_attrs, all_attributes, dm_inputs, mask):
+    '''
+    Write the measured surface, intensity, attributes, and inputs
+    to a single HDF5 file.
+
+    Attempting to write out the Mx dataset attributes (surface, intensity)
+    currently breaks things (Python crashes), so I've disabled that for now.
+    All the information *should* be in the attributes group, but it's
+    not as convenient.
+
+    Parameters:
+        filename: str
+            File to write out consolidate data to
+        surface_cube : nd array
+            Cube of surface images
+         surface_attrs : dict or h5py attributes object
+            Currently not used, but expected.
+         intensity_cube : nd array
+            Cube of intensity images
+        intensity_attrs : dict or h5py attributes object
+            Currently not used, but expected
+        all_attributes : dict or h5py attributes object
+            Mx attributes to associate with the file.
+        dm_inputs : nd array
+            Cube of inputs for the DM
+        mask : nd array
+            2D mask image
+    Returns: nothing
+    '''
+
+    # create hdf5 file
+    f = h5py.File(filename, 'w')
+    
+    # surface data and attributes
+    surf = f.create_dataset('surface', data=surface_cube)
+    #surf.attrs.update(surface_attrs)
+
+    intensity = f.create_dataset('intensity', data=intensity_cube)
+    #intensity.attrs.update(intensity_attrs)
+
+    attributes = f.create_group('attributes')
+    attributes.attrs.update(all_attributes)
+
+    dm_inputs = f.create_dataset('dm_inputs', data=dm_inputs)
+    #dm_inputs.attrs['units'] = 'microns'
+
+    mask = f.create_dataset('mask', data=mask)
+    
+    f.close()
+
+
+class FileMonitor(object):
+    '''
+    Watch a file for modifications at some
+    cadence and perform some action when
+    it's modified.  
+    '''
+    def __init__(self, file_to_watch):
+        '''
+        Parameters:
+            file_to_watch : str
+                Full path to a file to watch for.
+                On detecting a modificiation, do
+                something (self.on_new_data)
+        '''
+        self.file = file_to_watch
+        self.continue_monitoring = True
+
+        # Find initial state
+        self.last_modified = self.get_last_modified(self.file)
+
+    def watch(self, period=1.):
+        '''
+        Pick out new data that have appeared since last query.
+        Period given in seconds.
+        '''
+        self.continue_monitoring = True
+        try:
+            while self.continue_monitoring:
+                # Check the file
+                last_modified = self.get_last_modified(self.file)
+
+                # If it's been modified (and not deleted) perform
+                # some action and update the last-modified time.
+                if last_modified != self.last_modified:
+                    if os.path.exists(self.file):
+                        self.on_new_data(self.file)
+                    self.last_modified = last_modified
+
+                # Sleep for a bit
+                sleep(period)
+        except KeyboardInterrupt:
+            return
+
+    def get_last_modified(self, file):
+        '''
+        If the file already exists, get its last
+        modified time. Otherwise, set it to 0.
+        '''
+        if os.path.exists(file):
+            last_modified = os.stat(file).st_mtime
+        else:
+            last_modified = 0.
+        return last_modified
+
+    def on_new_data(self, newdata):
+        ''' Placeholder '''
+        pass
+
+class fourDMonitor(FileMonitor):
+    '''
+    Set the 4D machine to watch for an indication from
+    the DM that it's been put in the requested state,
+    and proceed with data collection when ready
+    '''
+    def __init__(self, path):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'dm_ready'
+                file indicating the DM is in the
+                requested state.
+        '''
+        super().__init__(os.path.join(path,'dm_ready'))
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting a new 'dm_ready' file,
+        stop blocking the 4D code. (No 
+        actual image capture happens here.)
+        '''
+        os.remove(newdata) # delete DM ready file
+        self.continue_monitoring = False # stop monitor loop
+
+class ZygoMonitor(FileMonitor):
+    '''
+    Set the Zygo machine to watch for an indication from
+    the DM that it's been put in the requested state,
+    and proceed with data collection when ready
+    '''
+    def __init__(self, path):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'dm_ready'
+                file indicating the DM is in the
+                requested state.
+        '''
+        super().__init__(os.path.join(path,'dm_ready'))
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting a new 'dm_ready' file,
+        stop blocking the Zygo code. (No 
+        actual image capture happens here.)
+        '''
+        os.remove(newdata) # delete DM ready file
+        self.continue_monitoring = False # stop monitor loop
+
+class BMC1KMonitor(FileMonitor):
+    '''
+    Set the DM machine to watch a particular FITS files for
+    a modification, indicating a request for a new DM actuation
+    state.
+
+    Will ignore the current file if it already exists
+    when the monitor starts (until it's modified).
+    '''
+    def __init__(self, path, input_file='dm_input.fits'):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'dm_input.fits'
+                file.
+        '''
+        super().__init__(os.path.join(path, input_file))
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting an updated dm_input.fits file,
+        load the image onto the DM and write out an
+        empty 'dm_ready' file to the network path
+        '''
+        # Load image from FITS file onto DM channel 0
+        log.info('Setting DM from new image file {}'.format(newdata))
+        load_channel(newdata, 0)
+
+        # Write out empty file to tell Zygo the DM is ready.
+        open(os.path.join(os.path.dirname(self.file), 'dm_ready'), 'w').close()
+
+class BMC2KMonitor(FileMonitor):
+    '''
+    Set the DM machine to watch a particular FITS files for
+    a modification, indicating a request for a new DM actuation
+    state.
+
+    Will ignore the current file if it already exists
+    when the monitor starts (until it's modified).
+    '''
+    def __init__(self, path, serial, input_file='dm_input.fits', script_path='/home/kvangorkom/BMC-interface'):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'dm_input.fits'
+                file.
+        '''
+        super().__init__(os.path.join(path, input_file))
+        self.serial = serial
+        self.script_path = script_path
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting an updated dm_input.fits file,
+        load the image onto the DM and write out an
+        empty 'dm_ready' file to the network path
+        '''
+        # Load image from FITS file onto DM channel 0
+        log.info('Setting DM from new image file {}'.format(newdata))
+        update_voltage_2K(newdata, self.serial, self.script_path)
+
+        # Write out empty file to tell Zygo the DM is ready.
+        open(os.path.join(os.path.dirname(self.file), 'dm_ready'), 'w').close()
+
+class ALPAOMonitor(FileMonitor):
+    '''
+    Set the DM machine to watch a particular FITS files for
+    a modification, indicating a request for a new DM actuation
+    state.
+
+    Will ignore the current file if it already exists
+    when the monitor starts (until it's modified).
+    '''
+    def __init__(self, path, serial, input_file='dm_input.fits'):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'dm_input.fits'
+                file.
+            serial : str
+                ALPAO DM97 serial number. Probably "BAX150"
+        '''
+        super().__init__(os.path.join(path, input_file))
+        self.serial = serial
+        #self.img = alpao.link_to_shmimage(serial)
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting an updated dm_input.fits file,
+        load the image onto the DM and write out an
+        empty 'dm_ready' file to the network path
+        '''
+        # Load image from FITS file onto DM channel 0
+        log.info('Setting DM from new image file {}'.format(newdata))
+        #alpao.apply_command(fits.open(newdata)[0].data, self.serial, self.img)
+        alpao.apply_command_from_fits(newdata, self.serial)
+
+        # Write out empty file to tell Zygo the DM is ready.
+        open(os.path.join(os.path.dirname(self.file), 'dm_ready'), 'w').close()
+
+class IrisAOMonitor(FileMonitor):
+    '''
+    Set the DM machine to watch a particular FITS files for
+    a modification, indicating a request for a new DM actuation
+    state.
+
+    Will ignore the current file if it already exists
+    when the monitor starts (until it's modified).
+    '''
+    def __init__(self, path, mserial, input_file='ptt_input.txt'):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'ptt_input.txt'
+                file.
+        '''
+        super().__init__(os.path.join(path, input_file))
+        self.mserial = mserial
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting an updated dm_input.fits file,
+        load the image onto the DM and write out an
+        empty 'dm_ready' file to the network path
+        '''
+        # Load image from FITS file onto DM channel 0
+        log.info('Setting DM from new PTT file {}'.format(newdata))
+        apply_ptt_command(newdata, mserial=self.mserial)
+
+        # Write out empty file to tell Zygo the DM is ready.
+        open(os.path.join(os.path.dirname(self.file), 'dm_ready'), 'w').close()
+
+class BaslerMonitor(FileMonitor):
+    def __init__(self, path, camera, images, stop_after_capture=False, nimages=1):
+        '''
+        Parameters:
+            path : str
+                Network path to watch for 'dm_ready'
+                file indicating the DM is in the
+                requested state.
+            camera : pypylon camera object
+            images : list
+                List to append images to
+            stop_after_capture : bool, opt.
+                Stop monitor after capturing an 
+                image? Default: False.
+            nimages : int, opt.
+                Take multiple images? If > 1, each element
+                of the image list will be an array of images
+        '''
+        super().__init__(os.path.join(path,'dm_ready'))
+        
+        self.camera = camera
+        self.images = images
+        self.stop_after_capture = stop_after_capture
+        self.nimages = nimages
+
+    def on_new_data(self, newdata):
+        '''
+        On detecting a new 'dm_ready' file, capture
+        an image on the Basler camera.
+        '''
+        if self.nimages == 1:
+            self.images.append(self.camera.grab_image().astype(float))
+        else:
+            self.images.append(np.asarray(list(self.camera.grab_images(self.nimages))).astype(float))
+        log.info('Grabbed Basler frame! ({})'.format(len(self.images)))
+        open(os.path.join(os.path.dirname(self.file), 'basler_ready'), 'w').close()
+        if self.stop_after_capture:
+            self.continue_monitoring = False
---
+++ fourD.py
@@ -0,0 +1,123 @@
+import os, sys
+
+import numpy as np
+# import h5py
+
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+#add 4D scripting directory to sys path
+#hard code it in for now
+
+try:
+    # Hard-coded path to Python scripting library on PhaseCam machine
+    sys.path.append('C:\\Program Files (x86)\\4Sight2.24\\scripts')
+    ''' 4Sight Python Script '''
+    from Scripting.App import *
+    from Scripting.Config import *
+    from Scripting.Data import *
+    from Scripting.Hardware import *
+    from Scripting.Measurements import *
+    from Scripting.Modify import *
+    from Scripting.Show import *
+    # connect to 4Sight session (4Sight must be open!)
+    # try:
+    #     connectionmanager.connect()
+    # except core.ZygoError:
+    #     log.warning('Zygo library loaded but connection to Mx could not be established.')
+except ImportError:
+    log.warning( 'Could not load 4Sight Python library!' )
+    log.warning( 'Functionality will be severely crippled.')
+
+def capture_frame(rawfolder,filenameprefix=None, mtype='acquire'):
+    '''
+    Capture an image on the PhaseCam via 4Sight. Saves to disk.
+
+    Parameters:
+        rawfolder : str (TODO optional?)
+            Location where frame data is to be stored.
+        filenameprefix : str (optional)
+            Filename of output. If not provided, 4Sight will
+            capture the image and load it into the interface
+            but it's up to the user to use the GUI to save
+            it out.
+        mtype : str
+            'acquire' or 'measure'. 'Acquire' takes a measurement
+            without analyzing or updating the GUI (faster), while
+            'measure' takes a measurement, analyzes, and updates
+            the GUI (slower).
+
+    The output is a .h5 file that includes the raw surface 
+    (no Zernike modes removed, even if selected in 4Sight), intensity,
+    and 4Sight attributes.
+
+    It's expected that all capture parameters will be set manually
+    in 4Sight: exposure time, masks, etc.
+    '''
+    log.info('4Sight: capturing frame and acquiring from camera.')
+    num_meas = 1
+    if mtype.upper() == 'ACQUIRE':
+        num_acq_frames = acquire_frames(num_frames=num_meas, 
+                                    output_dir=rawfolder, 
+                                    interval=None, 
+                                    use_detector_area=True, 
+                                    filename_prefix=filenameprefix, 
+                                    show_progress=True,
+                                    )
+        MessageBox("%d of %d frames acquired" % (num_acq_frames, num_meas))
+
+    elif (mtype.upper() == 'MEASURE') or (mtype.upper() == 'MEASUREMENT'):
+        measurement = AverageMeasure(frameCount=10,mblk='raw')
+    else:
+        raise ValueError('Not understood! Did you mean "acquire" or "measure"?')
+
+    if filename is not None:
+        log.info('4Sight: writing out to {0}'.format(filename))
+        if not SaveMeasurement(data=measurement,filename=filename):
+            log.warning('Error saving the measurement')
+
+def save_surface(filename):
+    '''
+    Save the surface currently loaded in 4Sight out
+    as a hdf5 file. This may or may not remove Zernikes.
+    I need to figure that out.
+
+    Parameters:
+        filename : str
+            Filename to save surface out to (as a .datx)
+
+    This mostly serves as an example of how to grab data
+    from Mx control elements. control_path can be found
+    by right-clicking on a GUI element in Mx in choosing
+    the "control path"(?) option in the dropdown.
+
+    '''
+    control_path = ("MEASURE", "Measurement", "Surface", "Surface Data")
+    surface_control = ui.get_control(control_path)
+    surface_control.save_data(filename) # .datx?
+
+def read_hdf5(filename, mode='r'):
+    '''
+    Simple wrapper around h5py to load a file
+    up (just because I have a hard time remembering
+    the syntax).
+
+    Parameters:
+        filename : str
+            File to open
+        mode : str (optional)
+            Mode to open file in.
+    Return : nothing
+    '''
+    return h5py.File(filename, mode)
+
+def open_in_Mx(filename):
+    '''
+    Open a .datx file in Mx
+
+    Parameters:
+        filename : str
+            File path to open.
+    Returns: nothing
+    '''
+    mx.load_data(filename)
